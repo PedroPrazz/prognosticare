a36824d184f981f19307b180f475d3f233726d8a
Mauricio Cardoso Oliveira <mauriciocardosooliveira@gmail.com>
2023-10-07 01:01:07 -0300

    minha agenda com calendario e eventos

---
+++ lib/src/pages/schedule/my_schedule_screen.dart
@@ -0,0 +1,129 @@
+import 'dart:collection';
+import 'package:flutter/material.dart';
+import 'package:prognosticare/utils.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MySchedule extends StatefulWidget {
+  const MySchedule({super.key});
+
+  @override
+  State<MySchedule> createState() => _MyScheduleState();
+}
+
+class _MyScheduleState extends State<MySchedule> {
+  final ValueNotifier<List<Event>> _selectedEvents = ValueNotifier([]);
+
+  final Set<DateTime> _selectedDays = LinkedHashSet<DateTime>(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
+
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return kEvents[day] ?? [];
+  }
+
+  List<Event> _getEventsForDays(Set<DateTime> days) {
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _focusedDay = focusedDay;
+      if (_selectedDays.contains(selectedDay)) {
+        _selectedDays.remove(selectedDay);
+      } else {
+        _selectedDays.add(selectedDay);
+      }
+    });
+
+    _selectedEvents.value = _getEventsForDays(_selectedDays);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Meus Agendamentos'),
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          TableCalendar<Event>(
+            // locale: 'pt_BR',
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            eventLoader: _getEventsForDay,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            selectedDayPredicate: (day) {
+              return _selectedDays.contains(day);
+            },
+            onDaySelected: _onDaySelected,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+          ),
+          ElevatedButton(
+            child: Text(
+              'Limpar seleção',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            onPressed: () {
+              setState(() {
+                _selectedDays.clear();
+                _selectedEvents.value = [];
+              });
+            },
+          ),
+          const SizedBox(height: 8.0),
+          Expanded(
+            child: ValueListenableBuilder<List<Event>>(
+              valueListenable: _selectedEvents,
+              builder: (context, value, _) {
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 4.0,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: ListTile(
+                        onTap: () => print('${value[index]}'),
+                        title: Text('${value[index]}'),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

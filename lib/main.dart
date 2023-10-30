c324c465c8ef8ebccebfbcc273de6ee7dab46346
Mauricio Cardoso Oliveira <mauriciocardosooliveira@gmail.com>
2023-10-30 19:45:47 -0300

    Update 30/10

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:prognosticare/src/config/calendar_controller.dart';
 import 'package:prognosticare/src/config/custom_colors.dart';

453ce8766edb92480334892350e5a118a5ff1d47
Mauricio Cardoso Oliveira <mauriciocardosooliveira@gmail.com>
2023-10-31 19:13:45 -0300

    Update do nome do APP

--- lib/src/config/uri.dart
+++ lib/src/config/uri.dart
@@ -1,12 +1,12 @@
-class UriLocal{
+class UriLocal {
   static final url = 'http://localhost:8080';
 }
 
-class UriServidor{
+class UriServidor {
   static final url = 'http://127.0.0.1:8080';
 }
 
-class UriTest{
+class UriTest {
   static final url = 'http://20.25.55.231';
- // static final url = 'http://192.168.0.8:8083';
+  // static final url = 'http://192.168.0.8:8083';
 }

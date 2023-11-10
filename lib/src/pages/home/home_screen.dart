b36931e092b0ce9edcaedb34013e8447a03992d1
Mauricio Cardoso Oliveira <mauriciocardosooliveira@gmail.com>
2023-11-10 01:43:40 -0300

    reestilizada a tela home do reposnsavel e dependente

--- lib/src/pages/home/home_screen.dart
+++ lib/src/pages/home/home_screen.dart
@@ -1,16 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get/get.dart';
 import 'package:prognosticare/components/dialogs/change_password_dialog.dart';
 import 'package:prognosticare/src/config/custom_colors.dart';
 import 'package:prognosticare/src/models/profilesModel.dart';
-import 'package:prognosticare/src/navBar/CustomBottomNavigationBar.dart';
 import 'package:prognosticare/src/pages/auth/dependents.dart';
 import 'package:prognosticare/components/dialogs/prontuario_dialog.dart';
 import 'package:prognosticare/src/api/service/findby_id_service.dart';
 import 'package:prognosticare/src/pages/auth/info.dart';
 import 'package:prognosticare/src/pages/auth/sign_in_screen.dart';
 import 'package:prognosticare/src/models/pessoa_model.dart';
+import 'package:prognosticare/src/pages/eventos/scheduleEventoPage.dart';
+import 'package:prognosticare/src/pages/home/perfisRow.dart';
 import 'package:prognosticare/src/pages/profile/profile_tab.dart';
 import 'package:prognosticare/src/pages/schedule/my_schedule_screen.dart';
 
@@ -30,14 +31,6 @@ class _HomeScreenState extends State<HomeScreen> {
   String? nome;
   String? tipoResponsavel;
   Profile? profile;
-
-  final List<String> imageNames = [
-    'img-adolescente.png',
-    'img-adulto.png',
-    'img-crianca.png',
-    'img-gestante.png',
-    'img-idoso.png',
-  ];
 
   @override
   void initState() {
@@ -65,8 +58,7 @@ class _HomeScreenState extends State<HomeScreen> {
         return _buildDependenteScreen();
       }
     } else {
-      return Scaffold(
-      );
+      return Scaffold();
     }
   }
 
@@ -201,9 +193,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: ListView(
+        child: Column(
           children: [
-            // 1. Imagem Tema Saúde
             Container(
               height: 200.0,
               decoration: BoxDecoration(
@@ -214,100 +205,89 @@ class _HomeScreenState extends State<HomeScreen> {
                 borderRadius: BorderRadius.circular(15),
               ),
             ),
-            SizedBox(height: 20),
-
-            Stack(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                        color: CustomColors.customContrastColor, width: 2),
-                    borderRadius: BorderRadius.circular(10),
+            SizedBox(height: 30),
+            Expanded(
+              child: GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                childAspectRatio: 1.3,
+                children: [
+                  _buildGridItem(
+                    iconData: Icons.person,
+                    text: 'Meus Dados',
+                    onTap: () async {
+                      if (pessoa == null) {
+                        pessoa = await GetFindbyIDService.getFindbyID();
+                      }
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProfileTab(pessoa: pessoa!),
+                        ),
+                      );
+                    },
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Icon(
-                              CupertinoIcons.heart_solid,
-                              color: Colors.white,
-                              size: 50,
-                            ),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Icon(
-                              Icons.medical_information,
-                              color: Colors.white,
-                              size: 50,
-                            ),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Icon(
-                              Icons.medication,
-                              color: Colors.white,
-                              size: 50,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                  _buildGridItem(
+                    iconData: Icons.assignment,
+                    text: 'Meu Prontuário',
+                    onTap: () {
+                      ProntuarioDialog().prontuarioDialog(context);
+                    },
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: Center(
-                    child: Text(
-                      'INFORMAÇÕES',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
+                  _buildGridItem(
+                    iconData: Icons.group,
+                    text: 'Meus Dependentes',
+                    onTap: () async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ListDependents(),
+                        ),
+                      );
+                    },
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                    color: CustomColors.customContrastColor, width: 2),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              height: 150.0,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: imageNames.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.only(right: 5),
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Image.asset('assets/images/${imageNames[index]}'),
-                    ),
-                  );
-                },
+                  _buildGridItem(
+                    iconData: Icons.event,
+                    text: 'Eventos',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ScheduleEventoPage(),
+                        ),
+                      );
+                    },
+                  ),
+                  _buildGridItem(
+                    iconData: Icons.healing,
+                    text: 'Vacinas',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ScheduleEventoPage(),
+                        ),
+                      );
+                    },
+                  ),
+                  _buildGridItem(
+                    iconData: Icons.swap_horiz,
+                    text: 'Trocar Perfil',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PerfisRow(profiles: []),
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
             ),
           ],
         ),
       ),
-
-      //navbar
-      bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
 
@@ -385,133 +365,120 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(width: 5),
         ],
       ),
-      body: GridView.count(
-        shrinkWrap:
-            true, // Usar isso para garantir que o GridView não seja infinito
-        crossAxisCount: 2, // Define a quantidade de itens na horizontal
-        childAspectRatio: 1.5, // Ajusta a proporção dos itens
-        children: [
-          _buildGridItem(iconData: Icons.medication, text: 'Medications'),
-          _buildGridItem(iconData: Icons.access_alarm, text: 'Reminders'),
-          _buildGridItem(iconData: Icons.favorite, text: 'Measurements'),
-          _buildGridItem(iconData: Icons.calendar_today, text: 'Appointments'),
-          _buildGridItem(iconData: Icons.autorenew, text: 'Refills'),
-          _buildGridItem(iconData: Icons.local_hospital, text: 'Doctors'),
-        ],
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              height: 200.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/medical_prescription.png'),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 2,
+                childAspectRatio: 1.3,
+                children: [
+                  _buildGridItem(
+                    iconData: Icons.person,
+                    text: 'Meus Dados',
+                    onTap: () async {
+                      if (pessoa == null) {
+                        pessoa = await GetFindbyIDService.getFindbyID();
+                      }
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProfileTab(pessoa: pessoa!),
+                        ),
+                      );
+                    },
+                  ),
+                  _buildGridItem(
+                    iconData: Icons.event,
+                    text: 'Eventos',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ScheduleEventoPage(),
+                        ),
+                      );
+                    },
+                  ),
+                  _buildGridItem(
+                    iconData: Icons.healing,
+                    text: 'Vacinas',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ScheduleEventoPage(),
+                        ),
+                      );
+                    },
+                  ),
+                  _buildGridItem(
+                    iconData: Icons.swap_horiz,
+                    text: 'Trocar Perfil',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PerfisRow(profiles: []),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      // body: Padding(
-      //   padding: EdgeInsets.all(16.0),
-      //   child: ListView(
-      //     children: [
-      //       //Imagem Tema Saúde
-      //       Container(
-      //         height: 200.0,
-      //         decoration: BoxDecoration(
-      //           image: DecorationImage(
-      //             image: AssetImage('assets/images/medical_prescription.png'),
-      //             fit: BoxFit.cover,
-      //           ),
-      //           borderRadius: BorderRadius.circular(15),
-      //         ),
-      //       ),
-      //       SizedBox(height: 20),
-
-      //       //Três botões
-      //       Row(
-      //         mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //         children: [
-      //           ElevatedButton(
-      //             onPressed: () {},
-      //             child: Icon(
-      //               CupertinoIcons.heart_solid,
-      //               color: Colors.white,
-      //               size: 30,
-      //             ),
-      //           ),
-      //           ElevatedButton(
-      //             onPressed: () {},
-      //             child: Icon(
-      //               Icons.medical_information,
-      //               color: Colors.white,
-      //               size: 30,
-      //             ),
-      //           ),
-      //           ElevatedButton(
-      //             onPressed: () {},
-      //             child: Icon(
-      //               Icons.medication,
-      //               color: Colors.white,
-      //               size: 30,
-      //             ),
-      //           )
-      //         ],
-      //       ),
-      //       SizedBox(height: 20),
-      //       // 3. Carrossel 1
-      //       Container(
-      //         height: 150.0,
-      //         child: ListView.builder(
-      //           scrollDirection: Axis.horizontal,
-      //           itemCount: 5,
-      //           itemBuilder: (context, index) {
-      //             return Padding(
-      //               padding: EdgeInsets.only(right: 5),
-      //               child: Image.asset('assets/images/covid$index.png'),
-      //             );
-      //           },
-      //         ),
-      //       ),
-      //       SizedBox(height: 20),
-
-      //       // 4. Carrossel 2
-      //       Container(
-      //         height: 150.0,
-      //         child: ListView.builder(
-      //           scrollDirection: Axis.horizontal,
-      //           itemCount:
-      //               imageNames.length, // Usa a quantidade de nomes na lista
-      //           itemBuilder: (context, index) {
-      //             return Padding(
-      //               padding: EdgeInsets.only(right: 5),
-      //               child: GestureDetector(
-      //                 onTap: () {
-      //                   // Aqui você pode definir o que acontece ao tocar em uma imagem do carrossel
-      //                 },
-      //                 child: Image.asset(
-      //                     'assets/images/${imageNames[index]}'), // Usa os nomes da lista
-      //               ),
-      //             );
-      //           },
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
-
-      //navbar
-      bottomNavigationBar: CustomBottomNavigationBar(),
     );
   }
 }
 
-Widget _buildGridItem({required IconData iconData, required String text}) {
+Widget _buildGridItem(
+    {required IconData iconData,
+    required String text,
+    required VoidCallback onTap}) {
   return Card(
+    color: CustomColors.customSwatchColor,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10),
+      side: BorderSide(
+        color: CustomColors.customContrastColor,
+        width: 2.0,
+      ),
+    ),
     elevation: 2.0,
     child: InkWell(
-      onTap: () {
-        // Defina o que acontece quando o card é tocado
-      },
+      onTap: onTap,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Icon(iconData, size: 40),
-          SizedBox(height: 8),
+          Icon(
+            iconData,
+            size: 50,
+            color: Colors.white,
+          ),
+          SizedBox(height: 12),
           Text(
             text,
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 16,
+              fontSize: 20,
+              color: Colors.white,
             ),
           ),
         ],

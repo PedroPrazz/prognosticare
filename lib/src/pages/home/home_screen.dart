0cde7ef70a1722d40e9eb7b284229c4bbe355af4
Pedro Paulo <103196282+pdropaullo@users.noreply.github.com>
2023-11-12 12:01:20 -0300

    rota vacinas

--- lib/src/pages/home/home_screen.dart
+++ lib/src/pages/home/home_screen.dart
@@ -419,7 +419,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => ScheduleEventoPage(),
+                          builder: (context) => Vaccination(),
                         ),
                       );
                     },

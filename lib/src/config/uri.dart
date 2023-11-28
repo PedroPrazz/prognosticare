5c0fb98363a8d011ed62f1b838ce0c17f2304367
Mauricio Cardoso Oliveira <mauriciocardosooliveira@gmail.com>
2023-11-28 14:27:51 -0300

    teste notificação

--- lib/src/config/uri.dart
+++ lib/src/config/uri.dart
@@ -2,8 +2,10 @@ class UriLocal {
   static final url = 'http://localhost:8080';
 }
 
-class UriServidor{
-  static final url ='http://187.65.251.55:8090';
+class UriServidor {
+  static final url = 'http://187.65.251.55:8090';
+
+  // static final url ='http://187.65.251.55:8090';
   //static final url = 'http://192.168.0.21:8085';
   //static final url = 'http://20.25.55.231';
 }
